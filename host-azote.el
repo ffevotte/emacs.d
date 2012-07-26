f3bd90541d38c70652d9c8d4255ab4376eb913d6
François Févotte <fevotte@gmail.com>
2012-07-26 23:14:58 +0200

    use expand-region

--- host-azote.el
+++ host-azote.el
@@ -2,6 +2,7 @@
 ;; Local emacs configuration file for azote ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(add-to-list 'load-path "~/projets/git/expand-region.el")
 (setq ff/use-org t)
 
 ;; Local Variables:
