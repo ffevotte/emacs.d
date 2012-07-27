66ee5130a56e936cd086eed902dee41580aea0a0
François Févotte <fevotte@gmail.com>
2012-07-27 09:20:50 +0200

    Default groups for ibuffer

--- setup-ibuffer.el
+++ setup-ibuffer.el
@@ -10,6 +10,23 @@
 (setq ibuffer-show-empty-filter-groups nil
       ibuffer-marked-char ?✓)
 
+(setq ibuffer-saved-filter-groups
+      `(("default"
+         ("Terminals" (mode . term-mode))
+         ("emacs.d" (filename . ,(expand-file-name "~/.emacs.d/")))
+	 ("Help" (or (mode . Man-mode)
+                     (mode . woman-mode)
+                     (mode . Info-mode)
+                     (mode . Help-mode)
+                     (mode . help-mode)))
+         ("Emacs internal" (or (name . "*Messages*")
+                               (name . "*Completions*")
+                               (name . "*Helm log*")
+                               (name . "*helm recentf*")
+                               (name . "*ESS*")
+                               (name . "*Compile-Log*"))))))
+
+
 (add-hook 'ibuffer-mode-hook 'ff/ibuffer-mode-hook)
 (defun ff/ibuffer-mode-hook ()
   (ibuffer-switch-to-saved-filter-groups "default")
