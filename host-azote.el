7a6be2701b38ff2ed193a8f0617ebdf84e7f87ed
François Févotte <fevotte@gmail.com>
2012-08-26 01:15:50 +0200

    Use multiple-cursors.el

--- host-azote.el
+++ host-azote.el
@@ -6,7 +6,17 @@
 (setq ff/use-org t)
 
 ;; Magit configuration
-(setq magit-repo-dirs '("~/.etc" "~/.emacs.d" "~/projets/git/"))
+(setq magit-repo-dirs '("~/.etc" "~/.emacs.d" "~/projets/git/" "~/EDF/atelier"))
+
+;; Multiple cursors
+(add-to-list 'load-path "~/projets/git/multiple-cursors.el")
+(require 'multiple-cursors)
+(defalias 'mc 'mc/edit-lines)
+(global-set-key (kbd "C-S-SPC") 'set-rectangular-region-anchor)
+(define-key rectangular-region-mode-map (kbd "C-g")
+  (lambda ()
+    (interactive)
+    (execute-kbd-macro [?  backspace ?\C-  ?\C- ])))
 
 ;; Local Variables:
 ;;   mode: emacs-lisp
