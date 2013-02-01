a2ab3b5c0160ccb263eeb6e5a5f7794b397fd46f
François Févotte <fevotte@gmail.com>
2013-02-01 16:31:58 +0100

    LaTeX setup: use TeX-PDF-mode by default (avoids problems with latex-preview)

--- init-std.el
+++ init-std.el
@@ -239,13 +239,15 @@ turned on."
 
 ;; LaTeX
 
-;; Use \eqref for equation references
-(setq reftex-label-alist '(AMSTeX))
+(setq reftex-label-alist '(AMSTeX)) ;; Use \eqref for equation references
+(setq-default TeX-PDF-mode t)       ;; Use pdflatex by default
 
-;; Turn on `reftex-mode' and `LaTeX-math-mode'
+;; Turn on `reftex-mode', `LaTeX-math-mode' and `TeX-PDF-mode'
 (defun turn-on-latex-math-mode () (LaTeX-math-mode 1))
+(defun turn-on-TeX-PDF-mode () (TeX-PDF-mode 1))
 (add-hook 'LaTeX-mode-hook 'turn-on-latex-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook 'turn-on-TeX-PDF-mode)
 
 ;; Fix incompatibility between latex-preview and gs versions
 (setq preview-gs-options '("-q" "-dNOSAFER" "-dNOPAUSE" "-DNOPLATFONTS"
