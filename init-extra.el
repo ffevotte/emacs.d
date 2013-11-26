44f80ae07ddfefcc02919ecca28743dc670bb612
François Févotte <fevotte@gmail.com>
2013-11-26 12:13:41 +0100

    Exchange elisp sections and subsection in iMenu

--- init-extra.el
+++ init-extra.el
@@ -441,9 +441,9 @@ Example usage:
 (defun imenu-elisp-sections ()
   (setq imenu-prev-index-position-function nil)
   (add-to-list 'imenu-generic-expression
-               '("SubSections" "^;;; \\(.+\\)$" 1) t)
+               '("Sections" "^;;; \\(.+\\)$" 1) t)
   (add-to-list 'imenu-generic-expression
-               '("Sections" "^;;;; \\(.+\\)$" 1) t))
+               '("SubSections" "^;;;; \\(.+\\)$" 1) t))
 (add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
 
 
